f1764a6dd351966abe4d18ef02eb50266f60e731
Nicholas Hollander <Arkazex@Gmail.com>
2014-09-30 11:14:29 -0400

    Made it work
    Aw yiss

--- src/com/arkazex/robotics/stairmp3/Main.java
+++ src/com/arkazex/robotics/stairmp3/Main.java
@@ -1,15 +1,16 @@
 package com.arkazex.robotics.stairmp3;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
 
 public class Main {
 	
@@ -20,44 +21,31 @@ public class Main {
 		for(int i = 0; i < 100; i++) {
 			System.out.print("> ");
 			String[] results = in.nextLine().split(" ");
+			playSound(results[0]);
 			
-			if(results[0].equalsIgnoreCase("play")) {
-				playSound(results[1]);
-				
-				Clip[] array = clips.toArray(new Clip[clips.size()]);
-				for(int a = 0; a < array.length; a++) {
-					array[a].stop();
-					clips.remove(a);
-				}
-			}
-			if(results[0].equalsIgnoreCase("stop")) {
-				Clip[] array = clips.toArray(new Clip[clips.size()]);
-				for(int a = 0; a < array.length; a++) {
-					array[a].stop();
-					clips.remove(a);
-				}
+			Clip[] array = clips.toArray(new Clip[clips.size()]);
+			for(int a = 0; a < array.length; a++) {
+				array[a].stop();
+				clips.remove(a);
 			}
 		}
 		in.close();
 	}
-	
 	public static void playSound(final String file) {
 		Thread t2 = new Thread() {
-			try {
-				if(!new File(file).exists()) {
-					System.out.println("Could not find file with name \"" + file + "\"");
-					return;
+			@Override
+			public void run() {
+				try {
+					if(!new File(file).exists()) {
+						System.out.println("Could not find file with name \"" + file + "\"");
+						return;
+					}
+					InputStream in = new FileInputStream(file);
+					AudioStream as = new AudioStream(in);
+					AudioPlayer.player.start(as);
+				} catch(Exception e) {
+					e.printStackTrace();
 				}
-				Clip clip = AudioSystem.getClip();
-				AudioInputStream inputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(file)));
-				clip.open();
-				clip.start();
-				Thread.sleep(3000);
-				clip.stop();
-				clip.close();
-				inputStream.close();
-			} catch(Exception e) {
-				e.printStackTrace();
 			}
 		};
 		t2.start();
