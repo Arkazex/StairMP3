866c8ea1de4c51dce4c7dfaabada5738e0fe6019
Nicholas Hollander <Arkazex@Gmail.com>
2014-10-07 11:23:41 -0400

    It reads serial!
    And no longer plays sounds...

--- src/com/arkazex/robotics/stairmp3/Main.java
+++ src/com/arkazex/robotics/stairmp3/Main.java
@@ -1,54 +1,30 @@
 package com.arkazex.robotics.stairmp3;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-import javax.sound.sampled.Clip;
-
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
+import jssc.SerialPort;
 
 public class Main {
 	
-	private static List<Clip> clips = new ArrayList<Clip>();
-	
 	public static void main(String[] args) throws InterruptedException {
-		Scanner in = new Scanner(System.in);
-		while(true) {
-			System.out.print("> ");
-			String[] results = in.nextLine().split(" ");
-			for(int i = 0; i < results.length; i++) {
-				if(results[i] == "x") {
-					Thread.sleep(500);
-				} else if(results[i] == "xx") {
-					Thread.sleep(1000);
-				} else {
-					playSound("WAVs/Track" + results[i] + ".wav");
+		try {
+			System.out.println("Connecting to serial device...");
+			SerialPort serialPort = new SerialPort("COM3");
+			System.out.println("Configuring device...");
+			serialPort.openPort();
+			serialPort.setParams(9600, 8, 1, 0);
+			serialPort.writeBytes("Hello!".getBytes());
+			while(true) {
+				StringBuilder sb = new StringBuilder();
+				byte in = 0;
+				while(in != 0x0A) {
+					in = serialPort.readBytes(1)[0];
+					sb.append(new String(new byte[]{in}));
 				}
+				System.out.print(sb.toString());
 			}
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-	}
-	public static void playSound(final String file) {
-		Thread t2 = new Thread() {
-			@Override
-			public void run() {
-				try {
-					if(!new File(file).exists()) {
-						System.out.println("Could not find file with name \"" + file + "\"");
-						return;
-					}
-					InputStream in = new FileInputStream(file);
-					AudioStream as = new AudioStream(in);
-					AudioPlayer.player.start(as);
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
-			}
-		};
-		t2.start();
 	}
 }
--- src/com/arkazex/robotics/stairmp3/Player.java
+++ src/com/arkazex/robotics/stairmp3/Player.java
@@ -1,5 +1,30 @@
 package com.arkazex.robotics.stairmp3;
 
-public class Player extends Thread{
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+public class Player{
+
+	public static void playClip(final String file) {
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					if(!new File(file).exists()) {
+						System.out.println("Could not find file with name \"" + file + "\"");
+						return;
+					}
+					InputStream in = new FileInputStream(file);
+					AudioStream as = new AudioStream(in);
+					AudioPlayer.player.start(as);
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();
+	}
 }
